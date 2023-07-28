f153978afa8d938007ec41e31b161fb875ee3d05
Daniel J. Szelogowski <dszelogowski@gmail.com>
2023-07-28 13:41:16 -0500

    Update -- modified dataset to be numerically encoded

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,6 @@
 import os
 import mido
+import numpy as np
 import pandas as pd
 
 
@@ -11,6 +12,16 @@ def note_number_to_name(note_number):
     return note + str(octave)
 
 
+def key_signature_to_number(key_signature):
+    mapping = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+               'A#', 'B#', 'C#', 'D#', 'E#', 'F#', 'G#',
+               'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Fb', 'Gb',
+               'Am', 'Bm', 'Cm', 'Dm', 'Em', 'Fm', 'Gm',
+               'A#m', 'B#m', 'C#m', 'D#m', 'E#m', 'F#m', 'G#m',
+               'Abm', 'Bbm', 'Cbm', 'Dbm', 'Ebm', 'Fbm', 'Gbm']
+    return mapping.index(key_signature)
+
+
 def midi_to_dataframe(midi_file):
     """Converts a MIDI file to a pandas dataframe.
     The dataframe has the following columns:
@@ -18,7 +29,8 @@ def midi_to_dataframe(midi_file):
     - velocity: The velocity of the note
     - time: The time in seconds of the event
     - tempo: The tempo in beats per minute at the time of the event
-    - time_signature: The time signature at the time of the event
+    - time_signature_count: The time signature numerator at the time of the event
+    - time_signature_beat: The time signature denominator at the time of the event
     - key_signature: The key signature at the time of the event
     """
     mid = mido.MidiFile(midi_file)
@@ -45,37 +57,52 @@ def midi_to_dataframe(midi_file):
                 if msg.velocity > 0:
                     if current_time > last_event_time:
                         # There is a gap between the last event and this one, insert a rest
-                        events.append('rest')
+                        # events.append('rest')
+                        events.append(-1)
                         velocities.append(0)
                         times.append(last_event_time)
                         tempi.append(mido.tempo2bpm(current_tempo))
                         time_signatures.append(current_time_signature)
-                        key_signatures.append(current_key_signature)
-                    events.append(note_number_to_name(msg.note))
+                        # key_signatures.append(current_key_signature)
+                        key_signatures.append(key_signature_to_number(current_key_signature))
+                    # events.append(note_number_to_name(msg.note))
+                    events.append(msg.note)
                     velocities.append(msg.velocity)
                     times.append(current_time)
                     tempi.append(mido.tempo2bpm(current_tempo))
                     time_signatures.append(current_time_signature)
-                    key_signatures.append(current_key_signature)
+                    # key_signatures.append(current_key_signature)
+                    key_signatures.append(key_signature_to_number(current_key_signature))
                     last_event_time = current_time
             elif msg.type == 'set_tempo':
-                current_tempo = msg.tempo
+                current_tempo = msg.tempo  # May need to record in a meta_events list also
             elif msg.type == 'time_signature':
                 current_time_signature = f"{msg.numerator}/{msg.denominator}"
             elif msg.type == 'key_signature':
                 current_key_signature = msg.key
         current_time = 0
 
+    # Split the time signature into two arrays, one for the numerator and one for the denominator
+    time_signature_counts = []
+    time_signature_beats = []
+
+    for time_signature in time_signatures:
+        time_signature_counts.append(time_signature.split('/')[0])
+        time_signature_beats.append(time_signature.split('/')[1])
+
     df = pd.DataFrame({'event': events, 'velocity': velocities, 'time': times, 'tempo': tempi,
-                       'time_signature': time_signatures, 'key_signature': key_signatures})
+                       'time_signature_count': time_signature_counts, 'time_signature_beat': time_signature_beats,
+                       'key_signature': key_signatures})
     return df
 
 
 def transpose_df_to_row(dataframe):
     """Transpose a dataframe to a single row where each column is a 1D array from the original dataframe."""
+    np.set_printoptions(threshold=np.inf)
     df = pd.DataFrame()
     for column in dataframe.columns:
-        df[column] = [dataframe[column].to_numpy()]
+        # Turn each column into a 1D array, then turn the array into a string in the form "[1, 2, 3, ...]"
+        df[column] = [np.array2string(dataframe[column].to_numpy(), separator=',')]
     return df
 
 
@@ -89,16 +116,18 @@ def build_dataset(data_dir):
     return df
 
 
-def save_dataset_csv(dataframe, output_file):
-    dataframe.to_csv(output_file, index=False)
+def save_dataset(dataframe, output_file):
+    dataframe.to_csv(output_file, index=False, sep=';')
 
 
 if __name__ == "__main__":
     # data_path = os.path.join(os.getcwd(), r"Data\MIDI\VoiceParts\Tenor\Isolated\534_001393_tenT.mid")
     # df_mid = midi_to_dataframe(data_path)
     # pd.set_option('display.max_rows', None)
+    # pd.set_option('display.max_columns', None)
     # print(df_mid)
     # print(transpose_df_to_row(df_mid))
+    # quit()
 
     SOPRANO_PATH = os.path.join(os.getcwd(), r"Data\MIDI\VoiceParts\Soprano\Isolated")
     ALTO_PATH = os.path.join(os.getcwd(), r"Data\MIDI\VoiceParts\Alto\Isolated")
@@ -112,9 +141,9 @@ if __name__ == "__main__":
     df_bass = build_dataset(BASS_PATH)
 
     print("Saving datasets...")
-    save_dataset_csv(df_soprano, os.path.join(os.getcwd(), r"Data\Tabular\Soprano.csv"))
-    save_dataset_csv(df_alto, os.path.join(os.getcwd(), r"Data\Tabular\Alto.csv"))
-    save_dataset_csv(df_tenor, os.path.join(os.getcwd(), r"Data\Tabular\Tenor.csv"))
-    save_dataset_csv(df_bass, os.path.join(os.getcwd(), r"Data\Tabular\Bass.csv"))
+    save_dataset(df_soprano, os.path.join(os.getcwd(), r"Data\Tabular\Soprano.csv"))
+    save_dataset(df_alto, os.path.join(os.getcwd(), r"Data\Tabular\Alto.csv"))
+    save_dataset(df_tenor, os.path.join(os.getcwd(), r"Data\Tabular\Tenor.csv"))
+    save_dataset(df_bass, os.path.join(os.getcwd(), r"Data\Tabular\Bass.csv"))
 
     print("Complete!")
